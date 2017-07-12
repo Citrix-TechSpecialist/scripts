c47a8618044323ce5e2f29bf3b91bf78f44c6440
Citrix Technology Specialists <Americas-TechSpecialist@citrix.com>
2017-07-12 02:22:01 -0700

    added docker clean script
    Pulls from cloud init to help students clean their docker host env.

---
+++ dockerclean.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+# Kill all running containers.
+docker kill $(docker ps -q)
+
+# Delete all stopped containers.
+printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker ps -a -q)
+
+# Delete all untagged images.
+printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q -f dangling=true)
