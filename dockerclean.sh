010b7c242d2adba40831bf0f5b3a08126030b824
Citrix Technology Specialists <Americas-TechSpecialist@citrix.com>
2017-07-12 02:41:22 -0700

    removes all volumes

--- dockerclean.sh
+++ dockerclean.sh
@@ -5,5 +5,8 @@ docker kill $(docker ps -q)
 # Delete all stopped containers.
 printf "\n>>> Deleting stopped containers\n\n" && docker rm $(docker ps -a -q)
 
-# Delete all untagged images.
+# Delete all images.
 printf "\n>>> Deleting untagged images\n\n" && docker rmi $(docker images -q)
+
+# Delete all volumes
+docker volume rm $(docker volume ls -qf)
